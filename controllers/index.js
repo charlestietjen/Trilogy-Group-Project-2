9ce47c650942c927c5b442286712c407a3e8dba7
Charles T <tietjen.charles@gmail.com>
2022-02-22 21:45:43 -0500

    temporary html route to serve index.html

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiControllers = require('./api/userController');
+const htmlRoutes = require('./html//html')
 
 router.use('/api/', apiControllers);
+router.use('/', htmlRoutes);
 
 module.exports = router;
