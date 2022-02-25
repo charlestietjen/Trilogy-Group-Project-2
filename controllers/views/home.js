a350bc8f6f8cd544bfbc87fcb5d703292ae07cc9
Tinastouch <tinastouch@gmail.com>
2022-02-24 19:32:32 -0500

    login layout

--- controllers/views/home.js
+++ controllers/views/home.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
 router.get('/', (req, res) => {
     console.log('view route hit')
     res.render('landing')
 });
+
+
 
 module.exports = router;
