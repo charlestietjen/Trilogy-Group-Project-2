9f4bbde66533f9e25d9597741d18ae60e9a7a930
Charles <tietjen.charles@gmail.com>
2022-02-27 21:08:32 -0500

    Update index.js

--- controllers/views/index.js
+++ controllers/views/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const dashboardRoutes = require('./dashboardRoutes');
 const homeRoutes = require('./home');
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 
 
