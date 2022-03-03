bc1f5cde32de09793b7ea5e7af5339c95f5d1a42
Steve Dusome <stevedusome@gmail.com>
2022-03-02 21:55:37 -0500

    support button, login page, and edit field size

---
+++ public/js/support-button-response.js
@@ -0,0 +1,12 @@
+var supportBtnEl = document.querySelector("#support")
+var rbarEl = document.querySelector("#right-sidebar")
+
+function supportButton () {
+    rbarEl.innerHTML = `<div class = "flex center spacer" style = "align-items: center; height:75%;">
+    <p class = "spacer text-white fade tf-h1" style="text-align:center;">Support</p>
+    <button id="login-btn" href="mailto:support@spacedout.com" class=" spacer text-white-glow tf-h1">contact us</button>
+</div>`
+
+}
+
+supportBtnEl.addEventListener("click", supportButton);
